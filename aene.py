b6e37cd340f0badb6fd71a16f38d3477b86b1eef
hexyatina <93482167+hexyatina@users.noreply.github.com>
2021-12-12 17:31:11 +0200

    Create aene.py

---
+++ aene.py
@@ -0,0 +1,16 @@
+x = [randint(0,3) for ]i in range(10)]
+print(x)
+
+x = input().split()
+x = list(map(int, x))
+print(x)
+
+x = lamda x: y*4+6
+print(x(5))
+
+
+while i := input('familia') != '0':
+  surname.append(i)
+  i = input('familia')
+print(i)
+
